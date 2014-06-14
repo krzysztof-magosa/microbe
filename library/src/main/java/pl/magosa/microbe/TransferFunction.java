c9f395cac4f2ffd54ceda3891b260e08d0bfade2
Krzysztof Magosa <krzysztof@magosa.pl>
2014-06-14 11:37:13 +0200

    new: TransferFunction interface

---
+++ library/src/main/java/pl/magosa/microbe/TransferFunction.java
@@ -0,0 +1,17 @@
+package pl.magosa.microbe;
+
+/**
+ * (c) 2014 Krzysztof Magosa
+ */
+public interface TransferFunction {
+    abstract public double function(final double value);
+
+    /**
+     * Calculate derivative
+     * @param value It's supposed to be output from function()
+     * @return
+     */
+    abstract public double derivative(final double value);
+    abstract public double getUpperLimit();
+    abstract public double getLowerLimit();
+}
