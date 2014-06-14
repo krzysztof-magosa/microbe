dc6026ab44bab504545bdbd0c63d2e1199f641b1
Krzysztof Magosa <krzysztof@magosa.pl>
2014-06-14 13:33:26 +0200

    new: AND logic operation example

---
+++ examples/and/src/main/java/pl/magosa/microbe/examples/and/Main.java
@@ -0,0 +1,51 @@
+package pl.magosa.microbe.examples.and;
+
+import pl.magosa.microbe.Input;
+import pl.magosa.microbe.Neuron;
+import pl.magosa.microbe.StepTransferFunction;
+
+/**
+ * (c) 2014 Krzysztof Magosa
+ */
+public class Main {
+    protected static Neuron neuron;
+
+    private static void print() {
+        System.out.printf(
+            "(%.0f and %.0f) -> %.0f\n",
+            neuron.getInputs().get(0).getValue(),
+            neuron.getInputs().get(1).getValue(),
+            neuron.getOutput()
+        );
+    }
+
+    public static void main(String[] args) {
+        neuron = new Neuron();
+
+        neuron.setTransferFunction(new StepTransferFunction());
+        neuron.setThreshold(2);
+        neuron.createInputs(2, (Input input) -> {
+            input.setWeight(1);
+        });
+
+        neuron.getInputs().get(0).setValue(0.0);
+        neuron.getInputs().get(1).setValue(0.0);
+        neuron.activate();
+        print();
+
+        neuron.getInputs().get(0).setValue(1.0);
+        neuron.getInputs().get(1).setValue(0.0);
+        neuron.activate();
+        print();
+
+        neuron.getInputs().get(0).setValue(0.0);
+        neuron.getInputs().get(1).setValue(1.0);
+        neuron.activate();
+        print();
+
+        neuron.getInputs().get(0).setValue(1.0);
+        neuron.getInputs().get(1).setValue(1.0);
+        neuron.activate();
+        print();
+    }
+}
