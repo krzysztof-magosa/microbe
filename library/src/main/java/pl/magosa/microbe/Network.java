2d392540aa1b62400d59bc7f6357cdaf37c1c72a
Krzysztof Magosa <krzysztof@magosa.pl>
2014-06-22 21:47:18 +0200

    extend: Network - require getNeuronsMap() to be implemented by all subclasses

--- library/src/main/java/pl/magosa/microbe/Network.java
+++ library/src/main/java/pl/magosa/microbe/Network.java
@@ -1,5 +1,7 @@
 package pl.magosa.microbe;
 
+import java.util.HashMap;
+
 /**
  * (c) 2014 Krzysztof Magosa
  */
@@ -7,4 +9,5 @@ public abstract class Network {
     abstract public void run();
     abstract public void setValues(final double[] values);
     abstract public double[] getOutput();
+    abstract protected HashMap<String, Neuron> getNeuronsMap();
 }
