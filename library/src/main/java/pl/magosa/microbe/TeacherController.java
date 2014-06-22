f27d8b71bd37bf95519e80d4f01f30fabb50253d
Krzysztof Magosa <krzysztof@magosa.pl>
2014-06-22 21:54:51 +0200

    remove: TeacherController - don't use slope to take decision

--- library/src/main/java/pl/magosa/microbe/TeacherController.java
+++ library/src/main/java/pl/magosa/microbe/TeacherController.java
@@ -16,8 +16,6 @@ public class TeacherController {
     protected double learningRateDecRatio;
     protected double maximumErrorIncRatio;
     protected double maximumLearningRate;
-    protected int slopeMinEpoches;
-    protected int slopeMaxEpoches;
     protected boolean debug;
     protected int debugEveryNEpoches;
 
@@ -28,12 +26,9 @@ public class TeacherController {
     protected double slope;
     protected boolean hasSlope = false;
     protected long epoch;
-    protected SimpleRegression regression;
-    protected CircularFifoQueue<Double> slopeFifo;
 
     public TeacherController(final Teacher teacher) {
         this.teacher = teacher;
-        regression = new SimpleRegression();
 
         setGoal(0.05);
         setLearningRate(0.01);
@@ -41,8 +36,6 @@ public class TeacherController {
         setLearningRateDecRatio(0.70);
         setMaximumErrorIncRatio(1.04);
         setMaximumLearningRate(0.25);
-        setSlopeMinEpoches(20);
-        setSlopeMaxEpoches(100);
         setDebug(false);
         setDebugInterval(10);
     }
@@ -103,22 +96,6 @@ public class TeacherController {
         this.maximumLearningRate = maximumLearningRate;
     }
 
-    public void setSlopeMinEpoches(final int slopeMinEpoches) {
-        if (slopeMinEpoches <= 0) {
-            throw new RuntimeException("slopeMinEpoches must be higher than 0");
-        }
-
-        this.slopeMinEpoches = slopeMinEpoches;
-    }
-
-    public void setSlopeMaxEpoches(final int slopeMaxEpoches) {
-        if (slopeMaxEpoches <= 0) {
-            throw new RuntimeException("slopeMaxEpoches must be higher than 0");
-        }
-
-        this.slopeMaxEpoches = slopeMaxEpoches;
-    }
-
     public void setDebug(final boolean debug) {
         this.debug = debug;
     }
@@ -135,19 +112,11 @@ public class TeacherController {
         System.out.printf("Epoch          = % d\n", epoch);
         System.out.printf("Previous error = % .10f\n", lastError);
         System.out.printf("Current error  = % .10f\n", currentError);
-        if (hasSlope) {
-            System.out.printf("Error slope    = % .10f\n", slope);
-        }
-        else {
-            System.out.printf("Error slope    =  not yet\n");
-        }
-
         System.out.printf("Learning rate  = % .10f\n", learningRate);
         System.out.println();
     }
 
     public void train() {
-        slopeFifo = new CircularFifoQueue<>(slopeMaxEpoches);
         teacher.setLearningRate(learningRate);
 
         teacher.calculateSquaredErrorEpoch();
@@ -162,19 +131,6 @@ public class TeacherController {
             }
             currentError = teacher.getError();
 
-            slopeFifo.add(currentError);
-            if (slopeFifo.size() >= slopeMinEpoches) {
-                regression.clear();
-
-                int x = 0;
-                for (Double y : slopeFifo) {
-                    regression.addData(x++, y);
-                }
-
-                slope = regression.getSlope();
-                hasSlope = true;
-            }
-
             if ((currentError / lastError) > maximumErrorIncRatio) {
                 teacher.rollback();
                 learningRate = learningRate * learningRateDecRatio;
@@ -182,7 +138,7 @@ public class TeacherController {
                 currentError = currentErrorBackup;
                 lastError = lastErrorBackup;
             }
-            else if (hasSlope && (slope < -0.00001)) {
+            else if (currentError < lastError) {
                 learningRate = learningRate * learningRateIncRatio;
             }
 
