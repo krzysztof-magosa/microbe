67c87a9aaf284bdc57e982cc6eb8ab76eb09388a
Krzysztof Magosa <krzysztof@magosa.pl>
2014-06-24 20:32:38 +0200

    doc: PixelMatrix - add missing comments

--- library/src/main/java/pl/magosa/microbe/PixelMatrix.java
+++ library/src/main/java/pl/magosa/microbe/PixelMatrix.java
@@ -6,6 +6,8 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
+ * Class represents matrix of pixels
+ *
  * (c) 2014 Krzysztof Magosa
  */
 public class PixelMatrix {
@@ -13,6 +15,9 @@ public class PixelMatrix {
     protected int width;
     protected int height;
 
+    /**
+     * Creates new matrix with specified dimensions
+     */
     public PixelMatrix(final int width, final int height) {
         data = new Pixel[height][width];
 
@@ -49,6 +54,20 @@ public class PixelMatrix {
         return width * height;
     }
 
+    /**
+     * Transforms matrix into array suitable to be used as input of network.
+     * Specified lambda is executed on each pixel, returned array is appended to array.
+     *
+     * Example usage:
+     * double result[] = matrix.transformToArray((Pixel pixel) -> {
+     *     return new Double[] {
+     *         transferFunction.normalize(pixel.getRelativeLuminance(), 0.0, 1.0)
+     *     };
+     * });
+     *
+     * @param function Lambda which analysis each pixel and returns array
+     * @return Array which represents image
+     */
     public double[] transformToArray(Function<Pixel, Double[]> function) {
         ArrayList<Double> vector = new ArrayList<>();
 
