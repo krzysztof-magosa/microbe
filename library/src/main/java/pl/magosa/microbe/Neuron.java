5e526dca2f684bea0f0947c22c9fa94db486bfbf
Krzysztof Magosa <krzysztof@magosa.pl>
2014-06-24 20:32:17 +0200

    doc: Neuron - add missing comments

--- library/src/main/java/pl/magosa/microbe/Neuron.java
+++ library/src/main/java/pl/magosa/microbe/Neuron.java
@@ -5,6 +5,8 @@ import java.util.Random;
 import java.util.function.Consumer;
 
 /**
+ * Class represents sigle neuron
+ *
  * (c) 2014 Krzysztof Magosa
  */
 public class Neuron {
@@ -20,6 +22,9 @@ public class Neuron {
         inputs = new ArrayList<>();
     }
 
+    /**
+     * Sets transfer function for this neuron
+     */
     public void setTransferFunction(TransferFunction transferFunction) {
         this.transferFunction = transferFunction;
     }
@@ -28,22 +33,40 @@ public class Neuron {
         return transferFunction;
     }
 
+    /**
+     * Gets input omitting bias (if any)
+     * @param index Index of input, bias is not counted, so 0 is always first input
+     * @return Input object
+     */
     public Input getInput(final int index) {
         return inputs.get(hasBias() ? index+1 : index);
     }
 
+    /**
+     * Creates inputs
+     * @param count How many inputs should be created
+     */
     public void createInputs(final int count) {
         for (int i = 1; i <= count; i++) {
             createInput((Input input) -> {});
         }
     }
 
+    /**
+     * Creates inputs and initialise them using specified lambda
+     * @param count How many inputs should be created
+     * @param initFunction Lambda which initialise each input
+     */
     public void createInputs(final int count, Consumer<Input> initFunction) {
         for (int i = 1; i <= count; i++) {
             createInput(initFunction);
         }
     }
 
+    /**
+     * Creates one input initialise it using specified lambda
+     * @param initFunction Lambda which initialise input
+     */
     public void createInput(Consumer<Input> initFunction) {
         Input input = new Input();
         initFunction.accept(input);
@@ -74,6 +97,9 @@ public class Neuron {
         return sum;
     }
 
+    /**
+     * Calculates output of neuron.
+     */
     public void activate() {
         sum = -threshold;
 
