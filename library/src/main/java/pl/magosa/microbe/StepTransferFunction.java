126e4a6234626767d0439f78d9a5c1bc23a6e89f
Krzysztof Magosa <krzysztof@magosa.pl>
2014-07-25 23:32:14 +0200

    extend: StepTransferFunction - ability to set custom threshold

--- library/src/main/java/pl/magosa/microbe/StepTransferFunction.java
+++ library/src/main/java/pl/magosa/microbe/StepTransferFunction.java
@@ -6,8 +6,14 @@ package pl.magosa.microbe;
  * (c) 2014 Krzysztof Magosa
  */
 public class StepTransferFunction extends TransferFunction {
+    private double threshold = 0;
+
+    public void setThreshold(final double threshold) {
+        this.threshold = threshold;
+    }
+
     public double function(final double value) {
-        return (value >= 0) ? 1.0 : 0.0;
+        return (value >= threshold) ? 1.0 : 0.0;
     }
 
     public double derivative(final double value) {
